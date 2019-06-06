d43f17e6d02c6a8737ae395a897036da8bd8fd9d
Chris George <chrisgeorge.9@gmail.com>
2019-06-05 17:23:20 -0700

    navbar changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
 
     <FaGulp />
     
